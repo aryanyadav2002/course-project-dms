683d2f6a7d693678d9a5792b10dcebb0d69cdab9
aryanyadav2002 <98759389+aryanyadav2002@users.noreply.github.com>
2022-10-02 21:06:11 +0530

    Update settings.py

--- cpdms/settings.py
+++ cpdms/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-f62x4)!1c61^5s7g*9()7a%)j@($tb4r2g*9p0*o$*1f2!nf5b'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
